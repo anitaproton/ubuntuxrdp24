deb8e102590be6ecc5c3da64166abdcc9a90dff9
Zhenpeng Ge <zhenpeng.ge@qq.com>
2026-05-01 01:53:08 +0000

    support docker cli

--- ubuntu-desktop/docker_config/pre_install.sh
+++ ubuntu-desktop/docker_config/pre_install.sh
@@ -4,3 +4,4 @@ apt-get install -y sudo vim gedit locales gnupg2 wget curl zip lsb-release bash-
 apt-get install -y net-tools iputils-ping mesa-utils software-properties-common build-essential
 apt-get install -y python3 python3-pip python3-numpy
 apt-get install -y openssh-server openssl git git-lfs tmux
+apt-get install -y docker-ce-cli docker-compose-plugin
