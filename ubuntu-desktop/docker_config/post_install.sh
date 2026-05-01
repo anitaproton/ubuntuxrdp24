6b6b8c93c841027fce5825a491741237ac04e6ab
Zhenpeng Ge <zhenpeng.ge@qq.com>
2026-05-01 02:29:49 +0000

    update install script

--- ubuntu-desktop/docker_config/post_install.sh
+++ ubuntu-desktop/docker_config/post_install.sh
@@ -15,3 +15,19 @@ fi
 curl -fSL "https://github.com/coder/code-server/releases/download/v${CODE_VERSION}/code-server_${CODE_VERSION}_${arch}.deb" -o code-server.deb
 dpkg -i ./code-server.deb
 rm code-server.deb
+# Add Docker APT repository and Install docker cli
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+if [ "$codename" = "noble" ]; then
+    echo "Types: deb\n\
+Architectures: $arch\n\
+Signed-By: /etc/apt/keyrings/docker.asc\n\
+URIs: https://download.docker.com/linux/ubuntu\n\
+Suites: $codename\n\
+Components: stable" > /etc/apt/sources.list.d/docker.sources
+else
+    echo "deb [arch=$arch signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $codename stable" > /etc/apt/sources.list.d/docker.list
+fi
+apt-get update
+apt-get install -y docker-ce-cli docker-compose-plugin
